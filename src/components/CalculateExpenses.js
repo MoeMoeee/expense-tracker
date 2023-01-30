155f666c33373bb1e8aa5cc002d528edadb29c92
MoeMoeee <dangkhoa.140502@gmail.com>
2023-01-30 22:41:27 +0700

    new

--- src/components/CalculateExpenses.js
+++ src/components/CalculateExpenses.js
@@ -2,19 +2,19 @@ import React from 'react'
 
 export const CalculateExpenses = () => {
   return (
-    <div className='max-w-sm border-solid border-2 bg-slate-200 p-5 flex space-x-2 m-5 shadow-gray-50	'>
-        <div>
+    <div className='max-w-xs mx-auto text-center flex justify-center border-solid border-2 bg-slate-200 p-5 space-x-2 m-5 shadow-gray-50	'>
+        <div className='mr-8'>
             <h4>Income</h4>
             <p>
-                <span className='text-green'>+</span>
+                <span className='font-bold text-green-700 text-lg'>+</span>
                 $0.00
             </p>
         </div>
 
-        <div className='ml-7 flex-col place-content-end'>
+        <div className='mxto'>
             <h4>Expense</h4>
             <p>
-                <span className='text-red'>-</span>
+                <span className='font-bold text-red-700 text-lg'>-</span>
                 $0.00
             </p>
         </div>
