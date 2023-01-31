397b4d911638230e14500bb31eb042ca69c676aa
MoeMoeee <dangkhoa.140502@gmail.com>
2023-01-31 18:55:24 +0700

    minor fixes

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const AddTransaction = () => {
   return (
-    <div className='text-center max-w-sm mx-auto'>
+    <div className='text-center max-w-sm mx-auto mt-4'>
       <div className='text-lg font-bold border-l-0 border-r-0 border-t-0 border-solid border border-gray-400'>Add Transaction</div>
     </div>
     )
--- src/components/TransactionHistory.js
+++ src/components/TransactionHistory.js
@@ -3,10 +3,10 @@ import React from 'react'
 export const TransactionHistory = () => {
   return (
     <div className='text-center max-w-sm mx-auto'>
-        <div className='text-lg font-bold border-l-0 border-r-0 border-t-0 border-solid border border-gray-400'>History</div>
-        <div className='pl-2 flex grow my-10 text-white font-medium border-2 border-none bg-cyan-500 shadow-lg shadow-cyan-500/50'>
+        <div className='mb-4 text-lg font-bold border-l-0 border-r-0 border-t-0 border-solid border border-gray-400'>History</div>
+        <div className='pl-2 flex grow  text-white font-medium border-2 border-none bg-cyan-500 shadow-lg shadow-cyan-500/50'>
           Cash
-          <span className='pl-72'>$20.0</span>  
+        <span className='pl-72'>$20.0</span>
         </div>
     </div>
   )
