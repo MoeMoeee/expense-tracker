fd06f93a8d73c716a2763044156c42e54217459b
MoeMoeee <dangkhoa.140502@gmail.com>
2023-01-31 19:12:59 +0700

    minor fixes

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -2,8 +2,23 @@ import React from 'react'
 
 export const AddTransaction = () => {
   return (
-    <div className='text-center max-w-sm mx-auto mt-4'>
-      <div className='text-lg font-bold border-l-0 border-r-0 border-t-0 border-solid border border-gray-400'>Add Transaction</div>
+    <div className=' max-w-sm mx-auto mt-4'>
+      <div className='text-center text-lg font-bold border-l-0 border-r-0 border-t-0 border-solid border border-gray-400'>Add Transaction</div>
+      <div>
+        {/* Type */}
+        <div>
+          <span className='font-bold text-lg'>Transaction Type:</span>  
+        </div>
+        <div>
+          <span className='font-bold text-lg'>Amount: {"\n"}
+            
+          </span>
+          <span className='font-bold text-lg'>
+            (Positive is Income, Negative is Expense)
+          </span>
+        </div>
+
+      </div>
     </div>
     )
 }
