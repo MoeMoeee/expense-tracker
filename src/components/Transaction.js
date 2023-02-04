6532b94d48a2163fcb9a0f45fe295ef6c277c70d
MoeMoeee <dangkhoa.140502@gmail.com>
2023-02-04 19:54:26 +0700

    fix history components to display different colors with different price

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
 export const Transaction = ({transaction}) => {
-  return (
-    <div>
-        <div className='mb-2 pl-2 flex grow  text-white font-medium border-2 border-none bg-cyan-500 shadow-lg shadow-cyan-500/50'>{transaction.text}
-        <span className='pl-72'>{transaction.amount}</span>
-        </div>
+  if (transaction.amount > 0) {
+    return (
+      <div className='mb-2 pl-4 flex grow  text-white font-medium border-2 border-none bg-cyan-500 shadow-lg shadow-cyan-500/50'>{transaction.text}
+        <span className='ml-auto mr-6'>${transaction.amount}</span>
+      </div>
+    )
+  }
+
+  else {
+    return (
+    <div className='mb-2 pl-4 flex grow  text-white font-medium border-2 border-none bg-red-500 shadow-lg shadow-cyan-500/50'>{transaction.text}
+      <span className='ml-auto mr-6'>-${-transaction.amount}</span>
     </div>
-  )
+    )
+  }
 }
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -3,8 +3,8 @@ import AppReducer from "./AppReducer";
 
 const initialState = {
     transactions: [
-    {id: 1, text: 'cash', amount: 20 },
-    {id: 2, text: 'expense', amount:-20 }
+    {id: 1, text: 'cash', amount: 2 },
+    {id: 2, text: 'expense', amount:-20000 }
     ]
 }
 
