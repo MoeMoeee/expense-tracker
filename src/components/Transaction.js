e29c2c35bcd7f9c2ac5fe6b53ea9ab7fb6d5d9b5
MoeMoeee <dangkhoa.140502@gmail.com>
2023-02-05 12:27:53 +0700

    added margin for delete button

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -5,11 +5,11 @@ export const Transaction = ({transaction}) => {
     return (
       <>
         <div className="mb-2 pl-4 flex grow text-white font-medium border-2 border-none bg-cyan-500 shadow-lg shadow-cyan-500/50 hover:bg-cyan-600">{transaction.text}
-          <span className='ml-auto mr-6'>
+          <span className='ml-auto mr-3'>
             ${transaction.amount}
 
           </span>
-          <button class="ml-10 inline-flex items-center px-2 py-2 bg-red-600 hover:bg-red-700 text-white text-sm font-medium rounded-sm">
+          <button class="inline-flex items-center px-2 py-2 bg-red-600 hover:bg-red-700 text-white text-sm font-medium rounded-sm">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
               </svg>
@@ -26,8 +26,8 @@ export const Transaction = ({transaction}) => {
   else {
     return (
     <div className='mb-2 pl-4 flex grow  text-white font-medium border-2 border-none bg-red-400 shadow-lg shadow-cyan-500/50'>{transaction.text}
-      <span className='ml-auto mr-6'>-${-transaction.amount}</span>
-      <button class="ml-0.5 inline-flex items-center px-2 py-2 bg-red-600 hover:bg-red-800 text-white text-sm font-medium rounded-sm">
+      <span className='ml-auto mr-3'>-${-transaction.amount}</span>
+      <button class="inline-flex items-center px-2 py-2 bg-red-600 hover:bg-red-800 text-white text-sm font-medium rounded-sm">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
               </svg>
