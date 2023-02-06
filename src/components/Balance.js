56112b92e7de8c26d097020d0258bd25b895e2c7
MoeMoeee <dangkhoa.140502@gmail.com>
2023-02-06 14:24:04 +0700

    update total balance based on history

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const Balance = () => {
+  const {transactions} = useContext(GlobalContext);
+  const amounts = transactions.map(transaction => transaction.amount)
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  
   return (
     <div className='text-center mt-11'>
         <div className='text-3xl mt-4'>Your balance is:</div>
-        <div className='font-semibold text-3xl'>$0.00</div>
+        <div className='font-semibold text-3xl'>${total}</div>
     </div>
   )
 }
