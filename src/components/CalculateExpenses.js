41e7f3aae65ca5aecc907636c9e6f9cf3da1e0b4
MoeMoeee <dangkhoa.140502@gmail.com>
2023-02-07 20:24:33 +0700

    format code

--- src/components/CalculateExpenses.js
+++ src/components/CalculateExpenses.js
@@ -5,16 +5,15 @@ import { useContext } from 'react'
 export const CalculateExpenses = () => {
     const {transactions} = useContext(GlobalContext);
     const amounts = transactions.map(transaction => transaction.amount);
-    const total = amounts.reduce((acc, item) => (acc += item),0).toFixed(2);
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
     const income = amounts
         .filter(item => item > 0)
-        .reduce((acc, item) => (acc+=item), 0)
+        .reduce((acc, item) => (acc += item), 0)
         .toFixed(2);
 
     const expense = total - income;
     const finalExpense = Math.abs(expense).toFixed(2);
-
 
   return (
     <div className='max-w-xs mx-auto flex justify-center bg-gray-300 p-5 space-x-2 m-5 shadow-gray-50 rounded-lg'>
