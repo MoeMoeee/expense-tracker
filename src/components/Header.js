6e94006f230e697f8b3c2a31603ca1d74a380705
MoeMoeee <dangkhoa.140502@gmail.com>
2023-02-24 11:33:30 +1100

    minor fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <>nter
+    <>
     <h1 className="mt-3 font-bold text-center text-5xl">Track Your Expenses</h1>
     </>
   )
