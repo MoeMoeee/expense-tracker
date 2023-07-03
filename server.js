173b50a9f97bcd289f9574b0de1e3bddbe987125
Ba Dang Khoa Pham <dangkhoa.140502@gmail.com>
2023-07-03 19:39:28 +0700

    set up route express

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgran = require('morgan');
+
+dotenv.config({path: './config/config.env'})
+
+const app = express();
+
+app.get('/', (req, res) => res.send("LALA"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log(`Running on port ${PORT}`.blue.bold))
+
+
+
