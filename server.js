e51a43ab703292b728319585b24b79f43076df00
Ba Dang Khoa Pham <dangkhoa.140502@gmail.com>
2023-07-03 19:55:58 +0700

    added middleware

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const morgran = require('morgan');
 dotenv.config({path: './config/config.env'})
 
 const app = express();
+const transactions = require('./route/transaction')
 
+app.use('/api/v1/transactions', transactions)
 app.get('/', (req, res) => res.send("LALA"));
 
 const PORT = process.env.PORT || 3000;
