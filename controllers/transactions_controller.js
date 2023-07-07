b156b0745744c2e2b22f1c9e92682a6ab79a7d7c
Ba Dang Khoa Pham <dangkhoa.140502@gmail.com>
2023-07-07 23:14:51 +0700

    set up delete transaction

--- controllers/transactions_controller.js
+++ controllers/transactions_controller.js
@@ -20,7 +20,29 @@ exports.getTransactions = async (req, res, next) => {
 
 // delete transactions to route /api/v1/transactions/:id
 exports.deleteTransactions = async (req, res, next) => {
-    res.send('DELETE transaction');
+    try {
+        const transaction = await Transaction.findById(req.params.id);
+
+        if (transaction !== null) {
+            await transaction.deleteOne();
+            return res.status(200).json({
+                success: true,
+                id: req.params.id
+            });
+        }
+
+        return res.status(404).json({
+            success: false,
+            error: "Can't find transaction"
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: 'Server Error',
+            message: error.message
+        });
+    }
 }
 
 // post transactions to route /api/v1/transactions
