c4fa1ea693a85deed6b545d8fb0377d00609e964
Ba Dang Khoa Pham <dangkhoa.140502@gmail.com>
2023-07-12 13:41:07 +0700

    set up to deploy

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
@@ -19,6 +20,12 @@ if(process.env.NODE_ENV === 'development') {
 }
 
 app.use('/api/v1/transactions', transactions)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 app.get('/', (req, res) => res.send("LALA"));
 
