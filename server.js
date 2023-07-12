8773f88c5cf29569d9aa71f70205940cb7bb97c4
Ba Dang Khoa Pham <dangkhoa.140502@gmail.com>
2023-07-12 23:42:40 +0700

    added build folder to deploy

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use('/api/v1/transactions', transactions)
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => res.sendFile(path.resolve('client', 'build', 'index.html')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
 app.get('/', (req, res) => res.send("LALA"));
