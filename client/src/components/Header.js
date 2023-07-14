71513242bc2152663926aa27d223e382bb1b00ff
Ba Dang Khoa Pham <dangkhoa.140502@gmail.com>
2023-07-14 10:12:22 +0700

    minor fixes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
   return (
     <>
-      <h1 className="mt-3 font-bold text-center text-5xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">Track Your Expenses</h1>
+      <h1 className="mt-3 font-bold text-center text-4xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">Track Your Expenses</h1>
     </>
   )
 }
