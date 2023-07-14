d974ae9c96bdfb6bed5b4b47a53c5f2247a3d185
Ba Dang Khoa Pham <dangkhoa.140502@gmail.com>
2023-07-14 23:57:17 +0700

    added safelist in tailwind config

--- client/tailwind.js
+++ client/tailwind.js
@@ -6,7 +6,10 @@ module.exports = {
     'bg-blue-500',
     'text-center',
     'hover:opacity-100',
-    // ...
+    'sm:text-3xl', 
+    'md:text-4xl', 
+    'lg:text-5xl', 
+    'xl:text-6xl',
     'lg:text-right',
   ],
   theme: {
